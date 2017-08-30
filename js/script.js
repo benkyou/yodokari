db01f525190405ff6b786bdd127ec142b72f224b
Jakeoid <jake.triumphant@gmail.com>
2017-08-30 17:44:34 +1000

    refactor(*): Rename yokodari to yodokari
    PSA: Dont name your things after obscure Japanese things.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var correct = 1;
 
 var type = $(location).attr('hash');
 var type = type.substring(1, type.length);
-var API = "https://cdn.rawgit.com/benkyou/yokodari/50cc20a1/lists/" + type + ".json";
+var API = "https://cdn.rawgit.com/benkyou/yodokari/50cc20a1/lists/" + type + ".json";
 
-$(document).prop('title', 'yokodari — ' + type);
+$(document).prop('title', 'yodokari — ' + type);
 $('#content-header').text('Study your ' + type + ' skills.')
 
 $(document).ready(function () {
